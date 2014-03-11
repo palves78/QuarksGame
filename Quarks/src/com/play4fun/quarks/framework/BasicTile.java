779dabf1e9fde2a675852c2a517aa02901f1755d
Pedro Alves <pedro.alves@medidata.pt>
2014-03-11 19:19:15 +0000

    Created BasicTile Class

---
+++ Quarks/src/com/play4fun/quarks/framework/BasicTile.java
@@ -0,0 +1,56 @@
+package com.play4fun.quarks.framework;
+
+public class BasicTile{
+	
+	public enum Type{SOLID};
+	private Type type;
+	private int col;
+	private int row;
+	
+	public BasicTile(){
+		
+	}
+	
+	public BasicTile(int col, int row){
+		set(col,row);
+	}
+	
+	public BasicTile(int col, int row, Type type){
+		set(col,row);
+		this.type = type;
+	}
+	
+	public void setCol(int col){
+		this.col = col;
+	}
+
+	public int getCol(){
+		return col;
+	}	
+	
+	public int getRow(){
+		return row;
+	}	
+	
+	public void setRow(int row){
+		this.row = row;
+	}
+	
+	public void set(int col, int row){
+		this.col = col;
+		this.row = row;
+	}
+	
+	public BasicTile getTile(){
+		return this;
+	}
+	
+	public void setType(Type type){
+		this.type = type;
+	}
+	
+	public Type getType(){
+		return type;
+	}
+	
+}
